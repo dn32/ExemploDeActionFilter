28cbb52e188a30552ebb3e8772d4888ddf218b17
Marcelo Vieira de Souza <dn@dn32.com.br>
2022-09-29 09:51:52 -0300

    Filtro adicionado

---
+++ ExemploDeActionFilter/Controllers/SomenteAcessoInternoController.cs
@@ -0,0 +1,18 @@
+﻿using ExemploDeActionFilter.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExemploDeActionFilter.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [TypeFilter(typeof(FiltroDeAcessoInterno))] // Este é o filtro que barra tudo que é acesso externo por meio do FiltroDeAcessoInterno
+    [ApiExplorerSettings(IgnoreApi = true)] // Este atributo faz a API não aparecer no swagger
+    public class SomenteAcessoInternoController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Você está acessando um controller privado por IP!";
+        }
+    }
+}
---
+++ ExemploDeActionFilter/Filters/FiltroDeAcessoInterno.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ExemploDeActionFilter.Filters
+{
+    public class FiltroDeAcessoInterno : ActionFilterAttribute
+    {
+        private const string TokenDeAcessoInternoPermitido = "123";
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (context.HttpContext.Request.Headers.TryGetValue("tokenDeAcessoInterno", out var tokenDeAcessoInterno) && tokenDeAcessoInterno.Equals(TokenDeAcessoInternoPermitido))
+            {
+                base.OnActionExecuting(context);
+                return;
+            }
+
+            context.Result = new StatusCodeResult(StatusCodes.Status403Forbidden);
+            return;
+        }
+    }
+}
